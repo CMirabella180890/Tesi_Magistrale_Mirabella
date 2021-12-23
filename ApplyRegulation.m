76b991bbef11d16d1d35c1bd5d5d516d111e67c0
CMirabella180890 <claudio.mirabella90@gmail.com>
2021-12-23 11:49:33 +0100

    Update

--- ApplyRegulation.m
+++ ApplyRegulation.m
@@ -47,52 +47,85 @@ switch (Aircraft.Certification.Regulation.value)
         % =================================================================
         switch (Aircraft.Certification.Regulation.SubpartC.Flightloads.Airload_case.Attributes.case)
             case 'OPEN VSP'
-            % -----------------------------------------------------------------
-            % CHANGE DIRECTORY TO CALCULATES C_l = C_l(y)
-            cd .. 
-            cd utilities\OpenVSP
-            % cd utilities\v2
-            % The 'dir' variable contains working directory path saved as a
-            % char value
-            dir = pwd;
-            % Store working directory inside the log file
-            fprintf('-----------------');
-            fprintf('\n');
-            fprintf('### Current directory ###');
-            fprintf('\n');
-            fprintf('%s\n', dir);
+                % -----------------------------------------------------------------
+                % CHANGE DIRECTORY TO CALCULATES C_l = C_l(y)
+                cd .. 
+                cd utilities\OpenVSP
+                % cd utilities\v2
+                % The 'dir' variable contains working directory path saved as a
+                % char value
+                dir = pwd;
+                % Store working directory inside the log file
+                fprintf('-----------------');
+                fprintf('\n');
+                fprintf('### Current directory ###');
+                fprintf('\n');
+                fprintf('%s\n', dir);
 
-            %% STARTING OPEN VSP CALCULATION
-            % Main_UAS
-            % +++ ISTRUZIONE IMPORTANTE +++
-            disp(" ++++ STARTING OPEN VSP CALCULATIONS ++++ ");
-            diary off
-            Main_PROSIB_WT 
-            % +++ ISTRUZIONE IMPORTANTE +++
-            % LOADING DATA INSIDE VARIABLE STRUCT
-            % CHANGE DIRECTORY AND LOAD AERODATA
-            FromTableToStructAircraft
-            % CHANGE DIRECTORY TO CALCULATES SHEAR AND BENDING MOMENT
-            cd .. 
-            cd ..
-            cd ..
-            diary CMFlightLoads.txt
-            disp(" ++++ FIGURE 9 - OPEN VSP RESULTS ++++ ");
-            cd csvla
-            % The 'dir' variable contains working directory path saved as a
-            % char value
-            dir = pwd;
-            % Store working directory inside the log file
-            fprintf('-----------------');
-            fprintf('\n');
-            fprintf('### Current directory ###');
-            fprintf('\n');
-            fprintf('%s\n', dir);
-            % CALCULATE SHEAR, BENDING, AND TORSION MOMENT
-            CalcShearBendTorsMom
-            % CALCULATE UNSYMMETRICAL LOADS
-            CalcUnsymmLoads
-            % -----------------------------------------------------------------
+                % STARTING OPEN VSP CALCULATION
+                % Main_UAS
+                % +++ ISTRUZIONE IMPORTANTE +++
+                disp(" ++++ STARTING OPEN VSP CALCULATIONS ++++ ");
+                diary off
+                Main_PROSIB_WT 
+                % +++ ISTRUZIONE IMPORTANTE +++
+                % LOADING DATA INSIDE VARIABLE STRUCT
+                % CHANGE DIRECTORY AND LOAD AERODATA
+                FromTableToStructAircraft
+                % CHANGE DIRECTORY TO CALCULATES SHEAR AND BENDING MOMENT
+                cd .. 
+                cd ..
+                cd ..
+                diary CMFlightLoads.txt
+                disp(" ++++ FIGURE 9 - OPEN VSP RESULTS ++++ ");
+                cd csvla
+                % The 'dir' variable contains working directory path saved as a
+                % char value
+                dir = pwd;
+                % Store working directory inside the log file
+                fprintf('-----------------');
+                fprintf('\n');
+                fprintf('### Current directory ###');
+                fprintf('\n');
+                fprintf('%s\n', dir);
+                % CALCULATE SHEAR, BENDING, AND TORSION MOMENT
+                CalcShearBendTorsMom
+                % CALCULATE UNSYMMETRICAL LOADS
+                CalcUnsymmLoads
+                % -----------------------------------------------------------------
+            case 'SCHRENK'
+                % -----------------------------------------------------------------
+                % CHANGE DIRECTORY TO CALCULATES C_l = C_l(y)
+                cd .. 
+                cd schrenk
+                % cd utilities\v2
+                % The 'dir' variable contains working directory path saved as a
+                % char value
+                dir = pwd;
+                % Store working directory inside the log file
+                fprintf('-----------------');
+                fprintf('\n');
+                fprintf('### Current directory ###');
+                fprintf('\n');
+                fprintf('%s\n', dir);
+                
+                % STARTING SCHRENK'S METHOD CALCULATION
+                CalcSchrenk
+                
+                % -----------------------------------------------------------------
+                % CHANGE DIRECTORY TO APPLY AIRWORTHINESS RULES
+                cd .. 
+                cd csvla
+                % The 'dir' variable contains working directory path saved as a
+                % char value
+                dir = pwd;
+                % Store working directory inside the log file
+                fprintf('-----------------');
+                fprintf('\n');
+                fprintf('### Current directory ###');
+                fprintf('\n');
+                fprintf('%s\n', dir);
+                % -----------------------------------------------------------------
         end
         % =================================================================
         % CALCULATE HORIZONTAL TAIL LOADS 
